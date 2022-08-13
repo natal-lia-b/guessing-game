7a082144055e5ddf52a1c194d3e06239130b92c8
natal-lia-b <80335618+natal-lia-b@users.noreply.github.com>
2022-08-13 19:41:07 +0300

    feat: passed all the tests

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.value = null;
+        this.min = null;
+        this.max = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.value = Math.ceil((this.max + this.min) / 2);
+        return this.value;
     }
 
     lower() {
-
+        this.max = this.value;
     }
 
     greater() {
-
+        this.min = this.value;
     }
 }
 
